cdc95a259bf602a9f887b73a862832aa0dcb1c98
Johan Brichau <johan@yesplan.be>
2020-06-01 20:01:33 +0200

    a bit of code cleanup; changed to semi-automatic mode and file names using timestamps

--- src/Camerabuddy.ino
+++ src/Camerabuddy.ino
@@ -3,7 +3,7 @@
 #include "SdFat.h"
 
 SYSTEM_THREAD(ENABLED);
-SYSTEM_MODE(AUTOMATIC);
+SYSTEM_MODE(SEMI_AUTOMATIC);
 
 #define SD_CS D6
 #define ARDUCAM_CS A5
@@ -11,49 +11,55 @@ SYSTEM_MODE(AUTOMATIC);
 ArduCAM myCAM(OV5642, ARDUCAM_CS);
 SdFat SD(&SPI1);
 
-void setup()
-{
-  // Wire.setSpeed(CLOCK_SPEED_100KHZ);
+String errorstate = "NONE";
+double soc;
+
+void setup() {
   Wire.begin();
-  Serial.begin();
+  Serial.begin(921600);
 
   pinMode(ARDUCAM_CS, OUTPUT);
   pinMode(SD_CS, OUTPUT);
   digitalWrite(ARDUCAM_CS, HIGH);
   digitalWrite(SD_CS, HIGH);
+  
+  Particle.variable("errorstate", errorstate);
+  Particle.variable("soc",soc);
 
   SPI.begin();
   initializeCam();
   initializeSD();
+  //autofocus();
+
+  Time.zone(2);
+  Particle.connect();
 }
 
-void loop()
-{
+void loop() {
+  myCAM.clear_bit(ARDUCHIP_GPIO,GPIO_PWDN_MASK);
+  delay(1000);
   takePicture();
   savePicture();
-  delay(1000);
+  //writeToSerial();
+  soc = System.batteryCharge();
+  myCAM.set_bit(ARDUCHIP_GPIO,GPIO_PWDN_MASK);
+  delay(5000);
 }
 
-void initializeCam()
-{
+void initializeCam() {
   uint8_t vid, pid;
   uint8_t temp;
 
   while (1)
   {
     Serial.println("Checking for camera...");
-
-    //Check if the ArduCAM SPI bus is OK
     myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
     temp = myCAM.read_reg(ARDUCHIP_TEST1);
-    if (temp != 0x55)
-    {
-      Serial.println("SPI interface Error!");
+    if (temp != 0x55) {
+      registerError("Arducam SPI interface Error!");
       Serial.println("myCam.read_reg said " + String(temp));
       delay(5000);
-    }
-    else
-    {
+    } else {
       break;
     }
     Particle.process();
@@ -61,18 +67,15 @@ void initializeCam()
 
   while (1)
   {
-    //Check if the camera module type is OV5642
+    Serial.println("Checking if the camera module type is OV5642");
     myCAM.rdSensorReg16_8(OV5642_CHIPID_HIGH, &vid);
     myCAM.rdSensorReg16_8(OV5642_CHIPID_LOW, &pid);
-    if ((vid != 0x56) || (pid != 0x42))
-    {
-      Serial.println(F("Can't find OV5642 module!"));
+    if ((vid != 0x56) || (pid != 0x42)) {
+      registerError("Can't find OV5642 module!");
       delay(5000);
       continue;
-    }
-    else
-    {
-      Serial.println(F("OV5642 detected."));
+    } else {
+      Serial.println("OV5642 detected.");
       break;
     }
   }
@@ -89,71 +92,71 @@ void initializeCam()
   delay(100);
   myCAM.clear_fifo_flag();
   delay(100);
-
   myCAM.write_reg(ARDUCHIP_FRAMES, 0x00);
   delay(100);
-
   //myCAM.OV5642_set_JPEG_size(OV5642_320x240);
   myCAM.OV5642_set_JPEG_size(OV5642_1600x1200);
-  //myCAM.OV5642_set_JPEG_size(OV5642_640x480);    // ?
-
-  // wait a sec`
+  //myCAM.OV5642_set_JPEG_size(OV5642_640x480);
   delay(1000);
 }
 
-void initializeSD()
-{
+void initializeSD() {
   if (!SD.begin(SD_CS))
-  {
-    Serial.println("failed to open card");
-    return;
+    registerError("Failed to open SD card!");
+}
+
+void autofocus()
+{
+  uint8_t regX;
+  myCAM.wrSensorReg16_8(0x3022, 0x03);
+  while (1) {
+    // check status
+    myCAM.rdSensorReg16_8(0x3029, &regX);
+    if (regX == 0x10) {
+      Serial.println("ACK CMD Auto focus completed END");
+      break;
+    } // focus completed
+    else {
+      Serial.println("ACK CMD Auto focusing ...END");
+      delay(1000);
+    }
   }
 }
 
-void takePicture()
-{
+void takePicture() {
+  unsigned long start_time, last_publish;
+  int size;
   Serial.println("Taking a picture...");
 
   myCAM.flush_fifo();
   delay(100);
-
   myCAM.clear_fifo_flag();
   delay(100);
-
   myCAM.start_capture();
   delay(100);
 
-  unsigned long start_time = millis(),
-                last_publish = millis();
-
-  //  wait for the photo to be done
-  while (!myCAM.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK))
-  {
+  start_time = millis();
+  last_publish = millis();
+  while (!myCAM.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK)) {
     delay(10);
     unsigned long now = millis();
-    if ((now - last_publish) > 1000)
-    {
+    if ((now - last_publish) > 10000) {
+      Particle.process();
       last_publish = now;
     }
-
     if ((now - start_time) > 30000)
-    {
       break;
-    }
   }
   delay(100);
-
-  int length = myCAM.read_fifo_length();
-  Serial.println("Image size is " + String(length));
+  Serial.println("IMAGE TAKEN");
 }
 
-uint8_t savePicture()
-{
+uint8_t savePicture() {
   uint8_t temp = 0, temp_last = 0;
   uint32_t length = 0;
   static int i = 0;
   static int k = 0;
-  char str[8];
+  String fileName;
   File outFile;
   byte buf[256];
   bool is_header = false;
@@ -161,13 +164,11 @@ uint8_t savePicture()
   length = myCAM.read_fifo_length();
   Serial.print(F("The fifo length is :"));
   Serial.println(length, DEC);
-  if (length >= MAX_FIFO_SIZE) //8M
-  {
+  if (length >= MAX_FIFO_SIZE) {
     Serial.println("Over size.");
     return 0;
   }
-  if (length == 0) //0 kb
-  {
+  if (length == 0) {
     Serial.println(F("Size is 0."));
     return 0;
   }
@@ -215,14 +216,13 @@ uint8_t savePicture()
       is_header = true;
       myCAM.CS_HIGH();
       k = k + 1;
-      itoa(k, str, 10);
-      strcat(str, ".jpg");
-      outFile = SD.open(str, O_WRITE | O_CREAT | O_TRUNC);
-      if (!outFile)
-      {
-        Serial.println(F("File open failed"));
-        while (1)
-          ;
+      fileName = Time.format(Time.now(), "%Y%m%d-%H%M%S");
+      Serial.println(fileName);
+      outFile = SD.open(String(fileName + ".jpg"), O_WRITE | O_CREAT | O_TRUNC);
+      if (!outFile) {
+        registerError("File open failed");
+        initializeSD();
+        return 0;
       }
       myCAM.CS_LOW();
       myCAM.set_fifo_burst();
@@ -232,4 +232,57 @@ uint8_t savePicture()
   }
   myCAM.CS_HIGH();
   return 1;
+}
+
+uint8_t writeToSerial()
+{
+  uint8_t temp = 0, temp_last = 0;
+  uint32_t length = 0;
+  bool is_header = false;
+
+  length = myCAM.read_fifo_length();
+  Serial.println(length, DEC);
+  if (length >= MAX_FIFO_SIZE) //512 kb
+  {
+    Serial.println(F("ACK CMD Over size. END"));
+    return 0;
+  }
+  if (length == 0) //0 kb
+  {
+    Serial.println(F("ACK CMD Size is 0."));
+    return 0;
+  }
+  myCAM.CS_LOW();
+  myCAM.set_fifo_burst(); //Set fifo burst mode
+  while (length--)
+  {
+    temp_last = temp;
+    temp = SPI.transfer(0x00);
+    if (is_header == true)
+    {
+      Serial.write(temp);
+    }
+    else if ((temp == 0xD8) & (temp_last == 0xFF))
+    {
+      is_header = true;
+      Serial.println(F("ACK IMG END"));
+      Serial.write(temp_last);
+      Serial.write(temp);
+    }
+    if ((temp == 0xD9) && (temp_last == 0xFF)) //If find the end ,break while,
+      break;
+    delayMicroseconds(15);
+  }
+  myCAM.CS_HIGH();
+  is_header = false;
+  return 1;
+}
+
+void registerError(String error) {
+  Serial.println(error);
+  errorstate = error;
+}
+
+void clearError() {
+  errorstate = "NONE";
 }
