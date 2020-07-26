46081b450e9cc9ea73f39d84a08faee601608b15
Johan Brichau <johan@yesplan.be>
2020-07-26 14:22:46 +0200

    customizing the code

--- src/Camerabuddy.ino
+++ src/Camerabuddy.ino
@@ -2,21 +2,31 @@
 #include "memorysaver.h"
 #include "SdFat.h"
 
-SYSTEM_THREAD(ENABLED);
-SYSTEM_MODE(SEMI_AUTOMATIC);
-
 #define SD_CS D6
 #define ARDUCAM_CS A5
 
+SYSTEM_THREAD(ENABLED);
+SYSTEM_MODE(SEMI_AUTOMATIC);
+
+SystemSleepConfiguration sleepconfig;
+SystemSleepResult sleepresult;
+SystemPowerConfiguration powerconfig;
+
+PMIC pmic;
+char batteryString[64] = "Null";
+
+int active = 1;
+
 ArduCAM myCAM(OV5642, ARDUCAM_CS);
 SdFat SD(&SPI1);
-
 String errorstate = "NONE";
-double soc;
+double freeSpace;
+long lastupdatemillis;
 
 void setup() {
   Wire.begin();
   Serial.begin(921600);
+  setPowerConfiguration("solarcharge");
 
   pinMode(ARDUCAM_CS, OUTPUT);
   pinMode(SD_CS, OUTPUT);
@@ -24,31 +34,53 @@ void setup() {
   digitalWrite(SD_CS, HIGH);
   
   Particle.variable("errorstate", errorstate);
-  Particle.variable("soc",soc);
+  Particle.variable("freeSpace", freeSpace);
+  Particle.variable("active", active);
+  Particle.function("command", command);
 
   SPI.begin();
   initializeCam();
   initializeSD();
   //autofocus();
 
-  Time.zone(2);
+  Time.zone(2); // should actually be 1 and set DST too, but taking a shortcut for now
+  sleepconfig.mode(SystemSleepMode::STOP);
   Particle.connect();
+  lastupdatemillis = 0;
 }
 
 void loop() {
-  myCAM.clear_bit(ARDUCHIP_GPIO,GPIO_PWDN_MASK);
-  delay(1000);
-  takePicture();
-  savePicture();
-  //writeToSerial();
-  soc = System.batteryCharge();
-  myCAM.set_bit(ARDUCHIP_GPIO,GPIO_PWDN_MASK);
-  delay(5000);
+  long currentmillis;
+
+  if(active==1) {
+    myCAM.clear_bit(ARDUCHIP_GPIO,GPIO_PWDN_MASK);
+    delay(800);
+    takePicture();
+    savePicture();
+    myCAM.set_bit(ARDUCHIP_GPIO,GPIO_PWDN_MASK);
+  }
+
+  int currentHour = Time.hour();
+  if (Time.isValid() & (currentHour > 8 | currentHour < 3)) {
+    if(active==1)
+      activate("off");
+    delay(10000);
+  } else {
+    if(active==0)
+      activate("on");
+    currentmillis = millis();
+    if (lastupdatemillis + (10 * 60000) < currentmillis) {
+      lastupdatemillis = currentmillis;
+      freeSpace = 0.000512 * SD.vol()->freeClusterCount() * SD.vol()->blocksPerCluster();
+    }
+  }
 }
 
 void initializeCam() {
   uint8_t vid, pid;
   uint8_t temp;
+  myCAM.clear_bit(ARDUCHIP_GPIO,GPIO_PWDN_MASK);
+  delay(800);
 
   while (1)
   {
@@ -94,9 +126,8 @@ void initializeCam() {
   delay(100);
   myCAM.write_reg(ARDUCHIP_FRAMES, 0x00);
   delay(100);
-  //myCAM.OV5642_set_JPEG_size(OV5642_320x240);
-  myCAM.OV5642_set_JPEG_size(OV5642_1600x1200);
-  //myCAM.OV5642_set_JPEG_size(OV5642_640x480);
+  //myCAM.OV5642_set_JPEG_size(OV5642_1600x1200);
+  myCAM.OV5642_set_JPEG_size(OV5642_2592x1944);
   delay(1000);
 }
 
@@ -125,8 +156,7 @@ void autofocus()
 
 void takePicture() {
   unsigned long start_time, last_publish;
-  int size;
-  Serial.println("Taking a picture...");
+  //Serial.println("Taking a picture...");
 
   myCAM.flush_fifo();
   delay(100);
@@ -147,7 +177,6 @@ void takePicture() {
     if ((now - start_time) > 30000)
       break;
   }
-  delay(100);
   Serial.println("IMAGE TAKEN");
 }
 
@@ -188,7 +217,7 @@ uint8_t savePicture() {
       outFile.write(buf, i);
       //Close the file
       outFile.close();
-      Serial.println(F("OK"));
+      //Serial.println(F("OK"));
       is_header = false;
       myCAM.CS_LOW();
       myCAM.set_fifo_burst();
@@ -285,4 +314,69 @@ void registerError(String error) {
 
 void clearError() {
   errorstate = "NONE";
+}
+
+int activate(String arg) {
+  if(arg=="on") {
+    active = 1;
+    myCAM.clear_bit(ARDUCHIP_GPIO,GPIO_PWDN_MASK);
+  }
+  if(arg=="off") {
+    active = 0;
+    myCAM.set_bit(ARDUCHIP_GPIO,GPIO_PWDN_MASK);
+  }
+  return 0;
+}
+
+void setPowerConfiguration(String arg) {
+  powerconfig = SystemPowerConfiguration();
+  if(arg=="solarcharge") {
+    powerconfig
+      .batteryChargeCurrent(1024)
+      .batteryChargeVoltage(4112)
+      .powerSourceMinVoltage(5080)
+      .powerSourceMaxCurrent(900);
+  }
+  if(arg=="usbcharge") {
+    powerconfig
+      .batteryChargeCurrent(896)
+      .batteryChargeVoltage(4112)
+      .powerSourceMinVoltage(3880)
+      .powerSourceMaxCurrent(900);
+  }
+  System.setPowerConfiguration(powerconfig);
+}
+
+void setLed(String arg) {
+  if(arg=="on")
+    LEDSystemTheme::restoreDefault();
+  if(arg=="off") {
+    LEDSystemTheme theme;
+    theme.setColor(LED_SIGNAL_CLOUD_CONNECTED, 0x00000000);
+    theme.apply();
+  }
+}
+
+int command(String arg) {
+  if((arg=="solarcharge") | (arg=="usbcharge")) {
+    setPowerConfiguration(arg);
+    return 1; 
+  }
+  if(arg=="godark") {
+    setLed("off");
+    return 1;
+  }
+  if(arg=="ledon") {
+    setLed("on");
+    return 1;
+  }
+  if(arg=="cameraon") {
+    activate("on");
+    return 1;
+  }
+  if(arg=="cameraoff") {
+    activate("off");
+    return 1;
+  }
+  return -1;
 }
